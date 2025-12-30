ace99a25182c848b8fff287838f10a0c1ef05180
Adamya singh <adamyasingh22@gmail.com>
2025-12-30 22:52:27 +0530

    api documentation

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 
 connectDB();
 
 const app = express();
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "docs/swagger.yaml")
+);
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
@@ -14,6 +20,7 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/user.routes"));
 app.use("/api/appointments", require("./routes/appointment.routes"));
 app.use("/api/services", require("./routes/service.routes"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Doctor Appointment API Running");
@@ -23,3 +30,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
