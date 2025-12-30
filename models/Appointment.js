54e56270059bc8362d0053978d4296d2a042c0cf
Adamya singh <adamyasingh22@gmail.com>
2025-12-30 11:26:31 +0530

    model setup

---
+++ models/Appointment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    doctorType: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    comments: String,
+    report: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
